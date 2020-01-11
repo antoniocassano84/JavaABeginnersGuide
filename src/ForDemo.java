abeb1c68bf2a8f7ef391c94f17122aeb4a1ce787
antoniocassano84 <antonio.cassano984@gmail.com>
2020-01-11 07:16:33 +0000

    ForDemo.java added

---
+++ src/ForDemo.java
@@ -0,0 +1,17 @@
+/*
+   Demonstrates the for loop.
+
+   Call this file ForDemo.java.
+*/
+class ForDemo {
+  public static void main(String args[]) {
+    int count;
+
+    //for(count = 0; count < 5; count = count + 1)
+    for(count = 0; count < 5; count++)
+      System.out.println("This is count: " + count);
+
+    System.out.println("Done!");
+
+  }
+}

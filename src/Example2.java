d51d9b59711681d5f619bbc6638d3ed2851435a1
antoniocassano84 <antonio.cassano984@gmail.com>
2020-01-11 06:21:44 +0000

    Example2.java added

---
+++ src/Example2.java
@@ -0,0 +1,20 @@
+/*
+  This demonstrates a variable.
+
+  Call thia file Example2.java
+*/
+class Example2 {
+  public static void main(String args[]) {
+    int myVar1;  // this declares a variable
+    int myVar2;  // this declares another variable
+
+    myVar1 = 1024;  // this assigns 1024 to myVar1
+
+    System.out.println("myVar1 contains " + myVar1);
+
+    myVar2 = myVar1 / 2;
+
+    System.out.print("myvar2 contains myVar1 / 2: ");
+    System.out.println(myVar2);
+  }
+}

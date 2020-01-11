7aa0e95cf0dda7030fbc59a2f0681c5a78659ae9
antoniocassano84 <antonio.cassano984@gmail.com>
2020-01-11 06:35:38 +0000

    Example3.java added

---
+++ src/Example3.java
@@ -0,0 +1,27 @@
+/*
+  This program illustrates the differences
+  between int and double.
+
+  Call thia file Example3.java
+*/
+class Example3 {
+  public static void main(String args[]) {
+    int v;  // this declares an int variable
+    double x;  // this declares a floating-point variable
+
+    v = 10;  // assigns v the value 10
+
+    x = 10.0;  // assign x the value 10.0
+
+    System.out.println("Original value of v: " + v);
+    System.out.println("Original value of x: " + x);
+    System.out.println();  // print a blank line
+
+    // now divide both by 4
+    v = v / 4;
+    x = x / 4;
+
+    System.out.println("v after division: " + v);
+    System.out.println("x after division: " + x);
+  }
+}

479f98fc62c8723265fc908e087a531bcd7440a1
antoniocassano84 <antonio.cassano984@gmail.com>
2020-01-29 10:49:25 +0000

    RelLogOps.java added

---
+++ src/RelLogOps.java
@@ -0,0 +1,23 @@
+// Demonstrate the relational and logical operators.
+class RelLogOps {
+  public static void main(String args[]) {
+    int i, j;
+    boolean b1, b2;
+
+    i = 10;
+    j = 11;
+    if(i < j) System.out.println("i < j");
+    if(i <= j) System.out.println("i <= j");
+    if(i != j) System.out.println("i != j");
+    if(i == j) System.out.println("this won't be executed");
+    if(i >= j) System.out.println("this won't be executed");
+    if(i > j) System.out.println("this won't be executed");
+
+    b1 = true;
+    b2 = false;
+    if(b1 & b2) System.out.println("this won't be executed");
+    if(!(b1 & b2)) System.out.println("!(b1 & b2) is true");
+    if(b1 | b2) System.out.println("b1 | b2 is true");
+    if(b1 ^ b2) System.out.println("b1 ^ b2 is true");
+  }
+}

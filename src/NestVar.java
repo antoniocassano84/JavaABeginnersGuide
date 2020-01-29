19f07cb0521d27f8924f19f92811071b8c7fe3ad
antoniocassano84 <antonio.cassano984@gmail.com>
2020-01-29 10:26:19 +0000

    ModDemo.java added

--- src/NestVar.java
+++ src/NestVar.java
@@ -12,7 +12,7 @@ class NestVar {
     for(count = 0; count < 10; count++) {
       System.out.println("This is count: " + count);
 
-      int count; // illegal!!!
+      //int count; // illegal!!!
       for(count = 0; count < 2; count++)
         System.out.println("This program is in error!");
     }

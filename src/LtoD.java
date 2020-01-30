44ef3dfce36598c135623569e7f34c08f7c3ca92
antoniocassano84 <antonio.cassano984@gmail.com>
2020-01-30 07:26:19 +0000

    SideEffects.java added

---
+++ src/LtoD.java
@@ -0,0 +1,17 @@
+// Demonstrate automatic conversion from long to double.
+class LtoD {
+  public static void main(String args[]) {
+    long L;
+    double D;
+
+    L = 100123285L;
+    D = L;
+
+    System.out.println("L and D: " + L + " " + D);
+
+    // *** This will not compile ***
+    D = 100123285.0;
+    //L = D; // Illegal!!!
+    System.out.println("L and D: " + L + " " + D);
+  }
+}

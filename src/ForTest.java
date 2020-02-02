41a628dcd8051d316763e776439ce7bec613e5e0
antoniocassano84 <antonio.cassano984@gmail.com>
2020-02-02 07:55:01 +0000

    ForTest.java added

---
+++ src/ForTest.java
@@ -0,0 +1,13 @@
+// Loop until an S is typed.
+class ForTest {
+  public static void main(String args[])
+    throws java.io.IOException {
+
+    int i;
+
+    System.out.println("Press S to stop.");
+
+    for(i = 0; (char) System.in.read() != 'S'; i++)
+      System.out.println("Pass #" + i);
+  }
+}

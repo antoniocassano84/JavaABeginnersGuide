60989646c3862f960b6ce9024eba3e0a569b37f4
antoniocassano84 <antonio.cassano984@gmail.com>
2020-02-02 08:58:45 +0000

    WhileDemo.java added

---
+++ src/WhileDemo.java
@@ -0,0 +1,13 @@
+// Demonstrate the while loop
+class WhileDemo {
+  public static void main(String args[]) {
+    char ch;
+
+    // print the alphabet using a while loop
+    ch = 'a';
+    while(ch <= 'z') {
+      System.out.println(ch);
+      ch++;
+    }
+  }
+}

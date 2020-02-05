7b088a2cd6d13bb4ef366244eec93dc68ef9daf7
antoniocassano84 <antonio.cassano984@gmail.com>
2020-02-05 20:18:20 +0000

    Vehicle.java added

---
+++ src/Vehicle.java
@@ -0,0 +1,7 @@
+// A program that uses the Vehicle class.
+
+class Vehicle {
+  int passengers; // number of passengers
+  int fuelcap;    // fuel capacity in gallons
+  int mpg;        // fuel consumption in miles per gallons
+}

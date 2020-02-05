9398574c003610bc2abd2a7f599f7fec2b1cb10c
antoniocassano84 <antonio.cassano984@gmail.com>
2020-02-05 20:24:32 +0000

    VehicleDemo.java added

---
+++ src/VehicleDemo.java
@@ -0,0 +1,17 @@
+// This class declares an object of type Vehicle.
+class VehicleDemo {
+  public static void main(String[] args) {
+    Vehicle minivan = new Vehicle();
+    int range;
+
+    // assign values to fields in minivan
+    minivan.passengers = 7;
+    minivan.fuelcap = 16;
+    minivan.mpg = 21;
+
+    // compute the range assuming a full tank of gas
+    range = minivan.fuelcap * minivan.mpg;
+    System.out.println("Minivan can carry " + minivan.passengers +
+                       " with a range of " + range);
+  }
+}

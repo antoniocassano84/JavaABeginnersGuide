499e63c5c1407d369ddacd774aa000e3b7fb409c
antoniocassano84 <antonio.cassano984@gmail.com>
2020-02-05 19:52:17 +0000

    Spaces.java added

---
+++ src/Spaces.java
@@ -0,0 +1,18 @@
+// Count spaces.
+class Spaces {
+  public static void main(String args[])
+    throws java.io.IOException {
+
+    char ch;
+    int spaces = 0;
+
+    System.out.println("Enter a period to stop.");
+
+    do {
+      ch = (char) System.in.read();
+      if(ch == ' ') spaces++;
+    }while(ch != '.');
+
+    System.out.println("Spaces: " + spaces);
+  }
+}

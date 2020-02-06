2acb45d0dd627855119e94fadbabc9ab8809c2ab
antoniocassano84 <antonio.cassano984@gmail.com>
2020-02-06 19:56:41 +0000

    TwoVehicles.java added again

--- src/TwoVehicles.java
+++ src/TwoVehicles.java
@@ -4,5 +4,26 @@ class TwoVehicles {
 
     Vehicle minivan = new Vehicle();
     Vehicle sportscar = new Vehicle();
+
+    int range1, range2;
+
+    // assign values to fields in minivan
+    minivan.passengers = 7;
+    minivan.fuelcap = 16;
+    minivan.mpg = 21;
+
+    // assign values to fields in sportscar
+    sportscar.passengers = 2;
+    sportscar.fuelcap = 14;
+    sportscar.mpg = 12;
+
+    // compute the range assuming a full tank of gas
+    range1 = minivan.fuelcap * minivan.mpg;
+    range2 = sportscar.fuelcap * sportscar.mpg;
+
+    System.out.println("Minivan can carry " + minivan.passengers +
+            " with a range of " + range1);
+    System.out.println("Sportscar can carry " + sportscar.passengers +
+            " with a range of " + range2);
   }
 }

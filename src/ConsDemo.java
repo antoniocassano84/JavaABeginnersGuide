f8bdba652bfb57535fc588291ee48941f9d7ef3c
antoniocassano84 <antonio.cassano984@gmail.com>
2020-02-08 06:29:10 +0000

    ParmConsDemo.java added

---
+++ src/ConsDemo.java
@@ -0,0 +1,8 @@
+class ConsDemo {
+  public static void main(String args[]) {
+    MyClass t1 = new MyClass();
+    MyClass t2 = new MyClass();
+
+    System.out.println(t1.x + " " + t2.x);
+  }
+}
---
+++ src/MyClass.java
@@ -0,0 +1,11 @@
+class MyClass {
+  int x;
+
+  MyClass() {
+    x = 10;
+  }
+
+  MyClass(int i) {
+    x = i;
+  }
+}

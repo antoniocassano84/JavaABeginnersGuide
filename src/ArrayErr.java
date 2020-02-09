02ae8aa64bff007e62ede9214a5f7e4cfa68dcb9
antoniocassano84 <antonio.cassano984@gmail.com>
2020-02-09 08:23:45 +0000

    ArrayErr.java added

---
+++ src/ArrayErr.java
@@ -0,0 +1,11 @@
+// Demonstrate an array overrun.
+class ArrayErr {
+  public static void main(String[] args) {
+    int sample[] = new int[10];
+    int i;
+
+    // generate an array overrun
+    for(i = 0; i < 100; i++)
+      sample[i] = i;
+  }
+}

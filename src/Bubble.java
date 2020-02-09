d53bde000dcda75660a5f9b5a3e0d36ae702e49c
antoniocassano84 <antonio.cassano984@gmail.com>
2020-02-09 12:52:47 +0000

    Bubble.java added

---
+++ src/Bubble.java
@@ -0,0 +1,45 @@
+/*
+   Try This 5-1
+
+   Demonstrate the Bubble sort.
+*/
+class Bubble {
+  public static void main(String args[]) {
+    int nums[] = { 99, -10, 100123, 18, -978,
+            5623, 463, -9, 287, 49 };
+    int a, b, t;
+    int size;
+
+    size = 10; // number of element to sort
+
+    //display original array
+    System.out.print("Original array is:");
+    displayArray(nums);
+
+    // This is the Bubble sort
+    int passN = 0;
+    for(a=1; a < size; a++) {
+      System.out.println("@@@ a is: " + a);
+      for(b=size-1; b >= a; b--) {
+        if(nums[b-1] > nums[b]) { // if out of order
+          // exchange elements
+          System.out.print("\t" + ++passN + ") Swapping " +
+                           nums[b-1] + " with "+ nums[b] + ": " );
+          t = nums[b-1];
+          nums[b-1] = nums[b];
+          nums[b] = t;
+          displayArray(nums);
+        }
+      }
+    }
+    // display sorted array
+    System.out.print("Sorted array is:");
+    displayArray(nums);
+  }
+
+  private static void displayArray(int[] nums) {
+    for(int i=0; i < nums.length; i++)
+      System.out.print(" " + nums[i]);
+    System.out.println();
+  }
+}

0e2444994ce783f770fb2e7ae18109a6dfdee92e
antoniocassano84 <antonio.cassano984@gmail.com>
2020-02-09 13:05:11 +0000

    Bubble.java added

--- src/Bubble.java
+++ src/Bubble.java
@@ -5,36 +5,24 @@
 */
 class Bubble {
   public static void main(String args[]) {
-    int nums[] = { 99, -10, 100123, 18, -978,
-            5623, 463, -9, 287, 49 };
-    int a, b, t;
-    int size;
-
-    size = 10; // number of element to sort
+    int nums[] = { 99, -10, 100123, 18, -978, 5623, 463, -9, 287, 49 };
+    int a, b, t, n;
 
     //display original array
-    System.out.print("Original array is:");
-    displayArray(nums);
+    System.out.print("Original array is: "); displayArray(nums);
 
     // This is the Bubble sort
-    int passN = 0;
-    for(a=1; a < size; a++) {
-      System.out.println("@@@ a is: " + a);
-      for(b=size-1; b >= a; b--) {
-        if(nums[b-1] > nums[b]) { // if out of order
-          // exchange elements
-          System.out.print("\t" + ++passN + ") Swapping " +
-                           nums[b-1] + " with "+ nums[b] + ": " );
-          t = nums[b-1];
-          nums[b-1] = nums[b];
-          nums[b] = t;
+    ;
+    for(a=1; a < nums.length; a++) // from index 1 to 8
+      for(n = 1, b=nums.length-1; b >= a; b--, n++) { // from index 9 to 'a'
+        if(nums[b-1] > nums[b]) { // if out of order exchange elements
+          System.out.print(a + " - " + n + ") Swapping " + nums[b-1] + " with "+ nums[b] + ": " );
+          t = nums[b-1]; nums[b-1] = nums[b]; nums[b] = t; // swapping
           displayArray(nums);
         }
       }
-    }
     // display sorted array
-    System.out.print("Sorted array is:");
-    displayArray(nums);
+    System.out.print("Sorted array is:"); displayArray(nums);
   }
 
   private static void displayArray(int[] nums) {
